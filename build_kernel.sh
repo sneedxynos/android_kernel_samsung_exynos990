ea4f38f00c602f72cf79924cada54be81a5a6ee5
doomvrr <doomurr@gmail.com>
2023-10-09 20:51:58 +0300

    use clang 17 and llvm

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,6 +4,8 @@ export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=T
 export ARCH=arm64
+export PATH=~/Kernel/linux-x86/clang-r487747c/bin:$PATH
+export LLVM=1 LLVM_IAS=1 CC=clang
 
 make exynos9830-r8slte_defconfig
 make -j4
